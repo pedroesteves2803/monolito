84eeb71ad466f698229c44493757b174f21298c1
Pedro Esteves <desenv3.ti@apcdcentral.com.br>
2024-09-06 17:33:41 -0300

    WIP

--- src/modules/product-adm/gateway/product.gateway.ts
+++ src/modules/product-adm/gateway/product.gateway.ts
@@ -2,5 +2,5 @@ import Product from "../domain/product.entity";
 
 export default interface ProductGateway {
     add(product: Product) : Promise<void>;
-    find(product: Product) : Promise<Product>;
+    find(id: string): Promise<Product>;
 }
